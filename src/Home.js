729111e3a6a3b144543ea4a5e39afe44d1e06ab5
Ken Nakamine <kennakamine@gmail.com>
2021-06-21 21:35:15 -0700

    feat(): merge jun's stuff

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, TouchableOpacity} from 'react-native';
+import { AntDesign } from '@expo/vector-icons'; 
 
 class Home extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home</Text>
-      </View>
-    );
+      <SafeAreaView style = {{flex: 1}}>
+        <ScrollView contentContainerStyle={{flexGrow: 1}}>
+          <View style = {{width: '90%', height: '11%', borderRadius: 10, alignSelf: 'center', marginTop: '3%', borderColor: 'black', backgroundColor: 'white', borderWidth: 2, padding: '2%'}}>
+            <View style = {{flexDirection: 'row', justifyContent:'space-between'}}>
+              <View>
+                <Text style = {{fontSize: 17, fontWeight: '500', paddingBottom: '2%'}}>
+                  Ken
+                </Text>
+                <Text style = {{fontSize: 17, fontWeight: '500'}}>
+                  Jun
+                </Text>
+              </View>
+              <View style = {{flexDirection: 'row'}}>
+                <View style = {{alignSelf: 'flex-end'}}>
+                  <Text style = {{fontSize: 17, fontWeight: '300', paddingBottom: '2%', paddingRight: '3%'}}>
+                    0
+                  </Text>
+                  <Text style = {{fontSize: 17, fontWeight: '500'}}>
+                    6
+                  </Text>
+                </View>
+                <View style = {{alignSelf: 'flex-end'}}>
+                  <Text style = {{fontSize: 17, fontWeight: '500', paddingBottom: '2%', paddingRight: '3%'}}>
+                    6
+                  </Text>
+                  <Text style = {{fontSize: 17, fontWeight: '300'}}>
+                    0
+                  </Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    )
   }
 }
 export default Home;
