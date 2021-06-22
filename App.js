dae494a3025706fefc47dec1aa9993c0ca897f1d
Ken Nakamine <kennakamine@gmail.com>
2021-06-21 20:58:06 -0700

    init

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './src/Home';
+
+/*
 export default function App() {
   return (
     <View style={styles.container}>
@@ -19,3 +25,39 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+*/
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home</Text>
+      </View>
+    );
+  }
+}
+export default Home;
